81d08501696ffb00edb3dffaf98a62e27b76ce25
nsbradford <nsbradford@gmail.com>
2016-02-10 07:19:06 -0800

    Sorting by n_fuel now works.

--- project3/simulate.c
+++ project3/simulate.c
@@ -32,24 +32,37 @@ typedef struct plane{
 	PlaneState state;
 } Plane;
 
+
 void print_plane(Plane p){
-	printf("id:%d\t t_start:%d\t maxfuel:%d\t is_emergency:%d\t state:%d\n", p.id, p.t_start, p.maxfuel, p.is_emergency, p.state);
+	printf("id:%d\t t_start:%d\t maxfuel:%d\t n_fuel:%d\tis_emergency:%d\t state:%d\n", 
+		p.id, p.t_start, p.maxfuel, p.n_fuel, p.is_emergency, p.state);
 }
+
+
+void print_all_planes(Plane planes[], unsigned int len){
+	int i;
+	for (i = 0; i < len; i++){
+		print_plane(planes[i]);
+	}
+}
+
 
 /**
  * Initialize each plane in the buffer with a random
  * t_start, maxfuel, and is_emergency.
  */
-void initialize_planes(Plane planes[], unsigned int l_buffer){
+void initialize_planes(Plane planes[], unsigned int len){
 	int i;
-	for (i = 0; i < l_buffer; i++){
+	for (i = 0; i < len; i++){
 		planes[i].id = i+1;
 		planes[i].t_start = rand() % T_START_RANGE + T_START_MIN;
 		planes[i].maxfuel = rand() % START_FUEL_RANGE + START_FUEL_MIN;
+		planes[i].n_fuel = planes[i].maxfuel;
 		planes[i].is_emergency = (float)rand()/(float)(RAND_MAX) > P_IS_EMERGENCY ? true : false;
 		planes[i].state = ARRIVING;
 	}
 }
+
 
 /**
  * Helper for qsort() in sort_plane_buffer().
@@ -60,11 +73,12 @@ int cmp_n_fuel(const void *a, const void *b){
 	return p1->n_fuel - p2->n_fuel;
 }
 
+
 /**
  * Sorts an array of Plane structs by increasing fuel remaining.
  */
-void sort_plane_buffer(Plane buffer[], unsigned int l_buffer){
-	qsort(buffer, l_buffer, sizeof(Plane), cmp_n_fuel);
+void sort_plane_buffer(Plane buffer[], unsigned int len){
+	qsort(buffer, len, sizeof(Plane), cmp_n_fuel);
 }
 
 
@@ -72,16 +86,14 @@ void sort_plane_buffer(Plane buffer[], unsigned int l_buffer){
  * Initialize simulate, and start threads.
  */
 int main(){
-	// initialize 25 planes
-	Plane plane_list[N_PLANES];
-	initialize_planes(plane_list, N_PLANES);
+	Plane planes[N_PLANES];
+	printf("Initialize planes...\n");
+	initialize_planes(planes, N_PLANES);
+	print_all_planes(planes, N_PLANES);
 
-	// begin simulation with the 25 planes
-	int i;
-	for (i = 0; i < N_PLANES; i++){
-		print_plane(plane_list[i]);
-	}
-
+	printf("Test sort by n_fuel...\n");
+	sort_plane_buffer(planes, N_PLANES);
+	print_all_planes(planes, N_PLANES);
 
 	return 0;
 }
