a9439474715ad56052799d92877e98497c1b594a
himanshusahay <himanshu23@icloud.com>
2016-02-26 17:45:05 -0500

    added test.c with multithreaded test

--- project4/test.c
+++ project4/test.c
@@ -13,16 +13,67 @@ void assertions(){
 	assert(HDD->size == 1000);
 }
 
+/**
+ * multithreaded_helper()
+ *
+ */
+void multithreaded_helper(int *threadID){
+	int i;
 
+	for (i=0; i<300; i++){
+		
+		int address = rand()%1000;
+		uint32_t *val = get_value(address);
+		*val = rand();
+		store_value(address, val);
+	}
+}
+
+/**
+ * test_multithreaded()
+ * Test the program with 5 threads
+ */
+
+void test_multithreaded(){
+
+	pthread_t threads[5];
+	vAddr indexes[1000];
+	int i;
+
+	// indexes
+	for (i = 0; i < 1000; ++i) {
+		if (DEBUG) 
+			printf("Page %d is being created\n",i);
+		
+		vAddr indexes[i] = create_page();
+		uint32_t *val = get_value(indexes[i]);
+		*val = (i * 3);
+		store_value(indexes[i], val);
+
+	}
+	
+	// threads
+	for(i=0; i<5; i++){
+		uint32_t *thread_id = malloc(sizeof(int));
+		*thread_id = i;
+		pthread_create (&(threads[i]),NULL, (void *) &multithreaded_helper, thread_id);
+	}
+}
+
+
+/**
+ * testRAM();
+ * This function puts everything into RAM and then successfully takes it out.
+ */
 void testRAM(){
 	printf("\n------------------------------\nTest testRAM()...\n");
 	vAddr indexes[10];
 	int i;
 	for (i = 0; i < 10; i++){
 		indexes[i] = create_page();
-		uint32_t *value = get_value(indexes[i]);
-		*value = (i * 3);
-		store_value (indexes[i], value);
+		uint32_t *val = get_value(indexes[i]);
+		*val = (i * 3);
+		store_value (indexes[i], val);
 	}
 	for (i = 0; i < 10; i++){
 		free_page(indexes[i]);
@@ -54,10 +105,18 @@ void memoryMaxer(){
  */
 int main(){
 	assertions();
-	//testRAM();
+	int i;
+	for (i = 1; i <= 3; i++){
+		EVICT_ALGO_NUMBER = i;
+	
+	testRAM();
 	memoryMaxer();
 	
 	// TODO multithreading
+	// !! check if this test works for multithreading
+	test_multithreaded();
 
+	}
+	
 	return 0;
 }
